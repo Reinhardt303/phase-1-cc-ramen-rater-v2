58df779ffbe949dfaae3db257b2308e0a5256c71
Thomas Rains <thomasrains01@gmail.com>
2024-11-21 18:43:39 -0800

    submits ramen rater challenge

--- src/index.js
+++ src/index.js
@@ -1,21 +1,60 @@
-// index.js
 
-// Callbacks
+const ramenMenuElement = document.getElementById('ramen-menu')
+const newRamenForm = document.getElementById('new-ramen')
+
 const handleClick = (ramen) => {
-  // Add code
-};
+  const imgElement = document.createElement('img')
+  imgElement.src = ramen.image
+  ramenMenuElement.appendChild(imgElement)
+  imgElement.addEventListener('click', () => {
+    displayRamens(ramen)
+  })
+}
+;
 
 const addSubmitListener = () => {
-  // Add code
+  newRamenForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newRamenNameInput = document.getElementById('new-name')
+    const newRamenImageInput = document.getElementById('new-image')
+    const newRamenRestaurant = document.getElementById('new-restaurant')
+    const newRamenRating = document.getElementById('new-rating')
+    const newRamenComment = document.getElementById('new-comment')
+  
+    const newRamen = {
+      name: newRamenNameInput.value,
+      image: newRamenImageInput.value,
+      restaurant: newRamenRestaurant.value,
+      rating: newRamenRating.value,
+      comment: newRamenComment.value
+    }
+    handleClick(newRamen)})
 }
 
-const displayRamens = () => {
-  // Add code
+const displayRamens = (ramens) => {
+  const ramenDetailImage = document.querySelector('.detail-image')
+  ramenDetailImage.src = ramens.image
+  const ramenDetailName = document.querySelector('.name')
+  ramenDetailName.innerHTML = ramens.name
+  const ramenDetailRestaurant = document.querySelector('.restaurant')
+  ramenDetailRestaurant.innerHTML = ramens.restaurant
+  const ramenDetailRating = document.getElementById('rating-display')
+  ramenDetailRating.innerHTML = ramens.rating
+  const ramenDetailComment = document.getElementById('comment-display')
+  ramenDetailComment.innerHTML = ramens.comment
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+fetch('http://localhost:3000/ramens')
+.then(response => response.json())
+.then(ramens => {
+  displayRamens(ramens[0])
+  ramens.forEach(ramen =>{ 
+        handleClick(ramen)
+  })
+})
+
+addSubmitListener()
 }
 
 main()
@@ -27,3 +66,57 @@ export {
   handleClick,
   main,
 };
+
+
+//step 1 grab ramen and render it at top likely displayRamens
+/* fetch('http://localhost:3000/ramens')
+.then(response => response.json())
+.then(ramens => {
+  displayRamenDetails(ramens[0])
+  ramens.forEach(ramen =>{ 
+        addFoodImageToRamenMenu(ramen)
+  })
+}) */
+
+
+/* function addFoodImageToRamenMenu(ramen){
+    const imgElement = document.createElement('img')
+    imgElement.src = ramen.image
+    ramenMenuElement.appendChild(imgElement)
+    imgElement.addEventListener('click', () => {
+      displayRamenDetails(ramen)
+    })
+} */
+/* function displayRamenDetails(ramens) {
+  const ramenDetailImage = document.querySelector('.detail-image')
+  ramenDetailImage.src = ramens.image
+  const ramenDetailName = document.querySelector('.name')
+  ramenDetailName.innerHTML = ramens.name
+  const ramenDetailRestaurant = document.querySelector('.restaurant')
+  ramenDetailRestaurant.innerHTML = ramens.restaurant
+  const ramenDetailRating = document.getElementById('rating-display')
+  ramenDetailRating.innerHTML = ramens.rating
+  const ramenDetailComment = document.getElementById('comment-display')
+  ramenDetailComment.innerHTML = ramens.comment
+} */
+
+
+//work below is to add new ramen using form
+
+/* newRamenForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const newRamenNameInput = document.getElementById('new-name')
+  const newRamenImageInput = document.getElementById('new-image')
+  const newRamenRestaurant = document.getElementById('new-restaurant')
+  const newRamenRating = document.getElementById('new-rating')
+  const newRamenComment = document.getElementById('new-comment')
+
+  const newRamen = {
+    name: newRamenNameInput.value,
+    image: newRamenImageInput.value,
+    restaurant: newRamenRestaurant.value,
+    rating: newRamenRating.value,
+    comment: newRamenComment.value
+  }
+  handleClick(newRamen)
+}) */
